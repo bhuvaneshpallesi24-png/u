bb19aa8626169840c3aa6223d9b01c2ca673a362
Bhuvanesh Pallesi <bhuvaneshpallesi24@gmail.com>
2026-03-30 09:54:09 +0530

    pushing main project to git

---
+++ app.py
@@ -0,0 +1,32 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+# 🔥 CONFIG - YOU CAN MODIFY THIS ANYTIME
+SECTIONS = {
+    "Docker": {
+        "Day 4 Revision": "docker-day4-revision.html",
+        "Day 3 Revision": "docker-day3-revision.html",
+        "Day 2 Revision": "docker-day2-revision.html",
+        "Day 1 Revision": "docker-day1-revision.html",
+        "Mastery Tracker": "docker-mastery-tracker.html",
+    },
+    "Kubernetes": {
+        "SRE Dashboard": "k8s-sre-dashboard.html",
+    },
+    "Python": {
+        "SRE Tracker": "sre_python_tracker.html",
+    }
+}
+
+@app.route("/")
+def home():
+    return render_template("layout.html", sections=SECTIONS)
+
+@app.route("/view/<section>/<subsection>")
+def view(section, subsection):
+    file_name = SECTIONS.get(section, {}).get(subsection)
+    return render_template("viewer.html", file=file_name, sections=SECTIONS)
+
+if __name__ == "__main__":
+    app.run(debug=True)
